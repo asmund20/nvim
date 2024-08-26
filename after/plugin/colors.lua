ce81f8c2c8a9cc516e3567ac60253728447804e8
Åsmund <asmund20@gmail.com>
2024-08-26 12:23:57 +0200

    Exported function so that the background can be set to transparent after packer syncing

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,6 +1,5 @@
-function ColorMyPencils(color)
-	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
+function ColorMyPencils()
+	vim.cmd.colorscheme("rose-pine")
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
@@ -8,3 +7,9 @@ function ColorMyPencils(color)
 end
 
 ColorMyPencils()
+
+vim.api.nvim_create_user_command(
+    'ColorMyPencils',
+    ColorMyPencils,
+    {}
+)
