92c6288a51726881e91238fdbcbdab4b593a1f38
asmund20 <asmund20@gmail.com>
2025-01-27 14:30:04 +0100

    Made tabs into two spaces in C to match the formatter

--- after/ftplugin/c.lua
+++ after/ftplugin/c.lua
@@ -1,2 +1,8 @@
 -- save, compile, run thun delete binary
 vim.keymap.set("n", "<leader>r", [[:w<Enter>:!gcc %<Enter>:!./a.out<Enter>:!rm a.out<CR>]])
+
+
+-- Set global tab-related options
+vim.opt_local.tabstop = 2       -- The number of spaces a <Tab> character displays as
+vim.opt_local.shiftwidth = 2    -- The number of spaces to use for each step of (auto)indent
+vim.opt_local.expandtab = true  -- Use spaces instead of tabs
