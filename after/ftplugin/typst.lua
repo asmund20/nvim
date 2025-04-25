b44c3ae8dba78e9bf110011beae936eb3cd6d534
asmund20 <asmund.sl@proton.me>
2025-04-25 21:51:16 +0200

    Add some normal mode keymappings

--- after/ftplugin/typst.lua
+++ after/ftplugin/typst.lua
@@ -7,7 +7,12 @@ vim.opt.expandtab = true
 -- Compile
 vim.keymap.set("n", "<leader>r", [[:wa<Enter>:!typst compile %<CR><CR>]])
 
+-- Normal mode preview
 vim.keymap.set("n", "<leader>t", ":TypstPreview<CR>")
+-- Slide mode preview
+vim.keymap.set("n", "<leader>ts", ":TypstPreview slide<CR>")
+-- Sync cursor
+vim.keymap.set("n", "<leader>tc", ":TypstPreviewSyncCursor<CR>")
 
 -- soft wrap
 vim.opt.wrap = true
