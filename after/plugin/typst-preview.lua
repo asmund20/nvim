ae0e01515c70106c9c53ab7ea483f8097c96c84c
Åsmund Olai Sand-Larsen <asmund.sl@proton.me>
2026-02-10 12:19:53 +0100

    Remove custom preview open command

--- after/plugin/typst-preview.lua
+++ after/plugin/typst-preview.lua
@@ -1,57 +1,57 @@
 require 'typst-preview'.setup {
-  -- Setting this true will enable printing debug information with print()
-  debug = false,
+    -- Setting this true will enable printing debug information with print()
+    debug = false,
 
-  -- Custom format string to open the output link provided with %s
-  -- Example: open_cmd = 'firefox %s -P typst-preview --class typst-preview'
-  open_cmd = 'zen %s',
+    -- Custom format string to open the output link provided with %s
+    -- Example: open_cmd = 'firefox %s -P typst-preview --class typst-preview'
+    -- open_cmd = 'zen %s',
 
-  -- Setting this to 'always' will invert black and white in the preview
-  -- Setting this to 'auto' will invert depending if the browser has enable
-  -- dark mode
-  invert_colors = '{"rest": "always","image": "never"}',
+    -- Setting this to 'always' will invert black and white in the preview
+    -- Setting this to 'auto' will invert depending if the browser has enable
+    -- dark mode
+    invert_colors = '{"rest": "always","image": "never"}',
 
-  -- Whether the preview will follow the cursor in the source file
-  follow_cursor = true,
+    -- Whether the preview will follow the cursor in the source file
+    follow_cursor = true,
 
-  -- Provide the path to binaries for dependencies.
-  -- Setting this will skip the download of the binary by the plugin.
-  -- Warning: Be aware that your version might be older than the one
-  -- required.
-  dependencies_bin = {
-    -- if you are using tinymist, just set ['typst-preview'] = "tinymist".
-    ['typst-preview'] = "tinymist",
-    ['websocat'] = nil
-  },
+    -- Provide the path to binaries for dependencies.
+    -- Setting this will skip the download of the binary by the plugin.
+    -- Warning: Be aware that your version might be older than the one
+    -- required.
+    dependencies_bin = {
+        -- if you are using tinymist, just set ['typst-preview'] = "tinymist".
+        ['typst-preview'] = "tinymist",
+        ['websocat'] = nil
+    },
 
-  -- This function will be called to determine the root of the typst project
-  get_root = function(path_of_main_file)
-    local root = os.getenv 'TYPST_ROOT'
-    if root then
-      return root
-    end
-    return vim.fn.fnamemodify(path_of_main_file, ':p:h')
-  end,
+    -- This function will be called to determine the root of the typst project
+    get_root = function(path_of_main_file)
+        local root = os.getenv 'TYPST_ROOT'
+        if root then
+            return root
+        end
+        return vim.fn.fnamemodify(path_of_main_file, ':p:h')
+    end,
 
-  -- This function will be called to determine the main file of the typst
-  -- project.
-  get_main_file = function(path_of_buffer)
-    return path_of_buffer
-  end,
+    -- This function will be called to determine the main file of the typst
+    -- project.
+    get_main_file = function(path_of_buffer)
+        return path_of_buffer
+    end,
 }
 
 vim.api.nvim_create_user_command('TypstPreviewLightMode', function()
-  local typst_preview = require("typst-preview")
-  
-  -- Get existing config and override just the invert_colors setting
-  local config = require("typst-preview.config")
-  local new_config = vim.tbl_deep_extend("force", config, {
-    invert_colors = 'never'
-  })
-  
-  typst_preview.setup(new_config)
-  
-  vim.cmd("TypstPreview")
+    local typst_preview = require("typst-preview")
 
-  typst_preview.setup(config)
+    -- Get existing config and override just the invert_colors setting
+    local config = require("typst-preview.config")
+    local new_config = vim.tbl_deep_extend("force", config, {
+        invert_colors = 'never'
+    })
+
+    typst_preview.setup(new_config)
+
+    vim.cmd("TypstPreview")
+
+    typst_preview.setup(config)
 end, {})
