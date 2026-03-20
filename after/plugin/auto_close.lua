1174e839c31bd481c6f1c7f69a3740be9bed2132
Åsmund Olai Sand-Larsen <asmund.sl@proton.me>
2026-03-20 12:11:06 +0100

    Stop auto-closing $ in anything that is not typst or markdown, also stop auto-closing ' in haskell

--- after/plugin/auto_close.lua
+++ after/plugin/auto_close.lua
@@ -1,24 +1,25 @@
 require("autoclose").setup({
-    keys = {
-        ["("] = { escape = false, close = true, pair = "()" },
-        ["["] = { escape = false, close = true, pair = "[]" },
-        ["{"] = { escape = false, close = true, pair = "{}" },
+	keys = {
+		["("] = { escape = false, close = true, pair = "()" },
+		["["] = { escape = false, close = true, pair = "[]" },
+		["{"] = { escape = false, close = true, pair = "{}" },
 
-        [">"] = { escape = true, close = false, pair = "<>" },
-        [")"] = { escape = true, close = false, pair = "()" },
-        ["]"] = { escape = true, close = false, pair = "[]" },
-        ["}"] = { escape = true, close = false, pair = "{}" },
+		[">"] = { escape = true, close = false, pair = "<>" },
+		[")"] = { escape = true, close = false, pair = "()" },
+		["]"] = { escape = true, close = false, pair = "[]" },
+		["}"] = { escape = true, close = false, pair = "{}" },
 
-        ['"'] = { escape = true, close = true, pair = '""' },
+		['"'] = { escape = true, close = true, pair = '""' },
+		["'"] = { escape = true, close = true, pair = "''", disabled_filetypes = { "haskell" } },
 
-        ["$"] = { escape = true, close = true, pair = "$$" },
-    },
-    options = {
-        disabled_filetypes = { "text" },
-        disable_when_touch = true,
-        touch_regex = "[%w(%[{]",
-        pair_spaces = false,
-        auto_indent = true,
-        disable_command_mode = false,
-    },
+		["$"] = { escape = true, close = true, pair = "$$", enabled_filetypes = { "typst", "markdown" } },
+	},
+	options = {
+		disabled_filetypes = { "text" },
+		disable_when_touch = true,
+		touch_regex = "[%w(%[{]",
+		pair_spaces = false,
+		auto_indent = true,
+		disable_command_mode = false,
+	},
 })
