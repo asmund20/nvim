586d2300015f68d60d4a99e7dbeed65f4bc2e3f0
asmund20 <asmund.sl@proton.me>
2026-09-03 15:11:06 +0200

    Add move lines to arrow keys as well

--- lua/bakugan/remap.lua
+++ lua/bakugan/remap.lua
@@ -4,6 +4,8 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "<S-Down>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<S-Up>", ":m '<-2<CR>gv=gv")
 
 -- Keep the cursor positioned in the middle while doing stuff
 vim.keymap.set("n", "J", "mzJ`z")
